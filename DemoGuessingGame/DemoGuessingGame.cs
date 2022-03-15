6498cb5e80c643615ec7f5016a2e91fd54e0764b
Aaron Stolle <aaronstolle@gmail.com>
2022-03-15 14:04:09 -0500

    Initial Commit

---
+++ DemoGuessingGame/DemoGuessingGame.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoGuessingGame
+{
+    public class StartUp
+    {
+        public void Run()
+        {
+            Console.WriteLine("Running");
+        }
+
+        public void CheckGuess()
+        {
+            // TODO: Guess a number between one and some max
+        }
+    }
+}
---
+++ DemoGuessingGame/View.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoGuessingGame
+{
+    public static void DisplayMessage(string message)
+    {
+        Console.WriteLine(message);
+    }
+
+    public static int GetGuess(int maxNum)
+    {
+        return Validation.PromptUser4Int("Enter your guess: ",1, maxNum)
+    }
+
+    public static int GetMaxNum()
+    {
+        return Validation.PromptUser4Int("Enter the max number to guess: ")
+    }
+}
